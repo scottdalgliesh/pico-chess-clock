31d28def6ea77fc760db52157d594e9ea757f179
Scott Dalgliesh <scott.t.dalgliesh@gmail.com>
2023-04-25 19:07:30 -0400

    add second button

--- src/main.rs
+++ src/main.rs
@@ -4,48 +4,57 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_rp::gpio::Pin;
 use embassy_rp::{gpio, peripherals};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::{Channel, Receiver, Sender};
 use embassy_time::{Duration, Timer};
-use gpio::{Input, Level, Output, Pull};
+use gpio::{AnyPin, Input, Level, Output, Pull};
 use {defmt_rtt as _, panic_probe as _};
 
 #[derive(Clone, Copy, Format)]
 enum ButtonEvent {
-    Pressed,
-    Released,
+    Pressed(Button),
+    Released(Button),
+}
+
+#[derive(Clone, Copy, Format)]
+enum Button {
+    Red,
+    Blue,
 }
 
 static CHANNEL: Channel<CriticalSectionRawMutex, ButtonEvent, 1> = Channel::new();
 
-#[embassy_executor::task]
+const DEBOUNCE_DELAY: u64 = 50;
+
+#[embassy_executor::task(pool_size = 2)]
 async fn button_watcher(
-    mut button: Input<'static, peripherals::PIN_15>,
+    mut button: Input<'static, AnyPin>,
+    button_id: Button,
     sender: Sender<'static, CriticalSectionRawMutex, ButtonEvent, 1>,
 ) {
     loop {
-        let debounce_delay = 50;
         button.wait_for_high().await;
-        sender.send(ButtonEvent::Pressed).await;
-        Timer::after(Duration::from_millis(debounce_delay)).await;
+        sender.send(ButtonEvent::Pressed(button_id)).await;
+        Timer::after(Duration::from_millis(DEBOUNCE_DELAY)).await;
 
         button.wait_for_low().await;
         info!("Button released");
-        sender.send(ButtonEvent::Released).await;
-        Timer::after(Duration::from_millis(debounce_delay)).await;
+        sender.send(ButtonEvent::Released(button_id)).await;
+        Timer::after(Duration::from_millis(DEBOUNCE_DELAY)).await;
     }
 }
 
 #[embassy_executor::task]
 async fn output_manager(
     mut led: Output<'static, peripherals::PIN_25>,
-    reciever: Receiver<'static, CriticalSectionRawMutex, ButtonEvent, 1>,
+    receiver: Receiver<'static, CriticalSectionRawMutex, ButtonEvent, 1>,
 ) {
     loop {
-        match reciever.recv().await {
-            ButtonEvent::Pressed => led.set_high(),
-            ButtonEvent::Released => led.set_low(),
+        match receiver.recv().await {
+            ButtonEvent::Pressed(_) => led.set_high(),
+            ButtonEvent::Released(_) => led.set_low(),
         }
     }
 }
@@ -54,15 +63,16 @@ async fn output_manager(
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let led = Output::new(p.PIN_25, Level::Low);
-    let button = Input::new(p.PIN_15, Pull::Down);
+    let red_button = Input::new(p.PIN_15.degrade(), Pull::Down);
+    let blue_button = Input::new(p.PIN_14.degrade(), Pull::Down);
     let sender = CHANNEL.sender();
-    let reciever = CHANNEL.receiver();
+    let receiver = CHANNEL.receiver();
 
     spawner
-        .spawn(button_watcher(button, sender.clone()))
+        .spawn(button_watcher(red_button, Button::Red, sender.clone()))
         .unwrap();
-
     spawner
-        .spawn(output_manager(led, reciever.clone()))
+        .spawn(button_watcher(blue_button, Button::Blue, sender.clone()))
         .unwrap();
+    spawner.spawn(output_manager(led, receiver)).unwrap();
 }
