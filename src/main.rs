4d7b7585caf58a2cdf7a0ed7f989ac7e1d640af6
Scott Dalgliesh <scott.t.dalgliesh@gmail.com>
2023-04-26 13:42:45 -0400

    refactor: move output manager to main

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_rp::gpio;
 use embassy_rp::gpio::Pin;
-use embassy_rp::{gpio, peripherals};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use embassy_sync::channel::{Channel, Receiver, Sender};
+use embassy_sync::channel::{Channel, Sender};
 use embassy_time::{Duration, Timer};
 use gpio::{AnyPin, Input, Level, Output, Pull};
 use {defmt_rtt as _, panic_probe as _};
@@ -40,33 +40,16 @@ async fn button_watcher(
         Timer::after(Duration::from_millis(DEBOUNCE_DELAY)).await;
 
         button.wait_for_low().await;
-        info!("Button released");
         sender.send(ButtonEvent::Released(button_id)).await;
         Timer::after(Duration::from_millis(DEBOUNCE_DELAY)).await;
-    }
-}
-
-#[embassy_executor::task]
-async fn output_manager(
-    mut red_led: Output<'static, peripherals::PIN_16>,
-    mut blue_led: Output<'static, peripherals::PIN_17>,
-    receiver: Receiver<'static, CriticalSectionRawMutex, ButtonEvent, 1>,
-) {
-    loop {
-        match receiver.recv().await {
-            ButtonEvent::Pressed(Button::Red) => red_led.set_high(),
-            ButtonEvent::Released(Button::Red) => red_led.set_low(),
-            ButtonEvent::Pressed(Button::Blue) => blue_led.set_high(),
-            ButtonEvent::Released(Button::Blue) => blue_led.set_low(),
-        }
     }
 }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let red_led = Output::new(p.PIN_16, Level::Low);
-    let blue_led = Output::new(p.PIN_17, Level::Low);
+    let mut red_led = Output::new(p.PIN_16, Level::Low);
+    let mut blue_led = Output::new(p.PIN_17, Level::Low);
     let red_button = Input::new(p.PIN_15.degrade(), Pull::Down);
     let blue_button = Input::new(p.PIN_14.degrade(), Pull::Down);
     let sender = CHANNEL.sender();
@@ -78,7 +61,14 @@ async fn main(spawner: Spawner) {
     spawner
         .spawn(button_watcher(blue_button, Button::Blue, sender.clone()))
         .unwrap();
-    spawner
-        .spawn(output_manager(red_led, blue_led, receiver))
-        .unwrap();
+
+    // manage outputs
+    loop {
+        match receiver.recv().await {
+            ButtonEvent::Pressed(Button::Red) => red_led.set_high(),
+            ButtonEvent::Released(Button::Red) => red_led.set_low(),
+            ButtonEvent::Pressed(Button::Blue) => blue_led.set_high(),
+            ButtonEvent::Released(Button::Blue) => blue_led.set_low(),
+        }
+    }
 }
